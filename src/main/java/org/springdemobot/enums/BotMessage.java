1d51b002bb1122f9a7562076a593102236cb4ae3
Savelkins <savelkains@gmail.com>
2025-06-25 08:45:17 +0300

    Commit

--- src/main/java/org/springdemobot/enums/BotMessage.java
+++ src/main/java/org/springdemobot/enums/BotMessage.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum BotMessage {
     HELP_MESSAGE("Вы можете выполнять команды в главном меню..."),
     WELCOME("Привет, рад встрече!"),
-    UNKNOWN_COMMAND("Прости, я не знаю эту команду.");
+    UNKNOWN_COMMAND("Прости, я не знаю эту команду."),
+    MUTED_FOR_PROFANITY("Ваше сообщение было удалено. Вы временно заглушены в чате за использование нецензурной лексики. Пожалуйста, соблюдайте правила.");
 
     private final String text;
 
--- src/main/java/org/springdemobot/service/UserService.java
+++ src/main/java/org/springdemobot/service/UserService.java
@@ -1,13 +1,19 @@
 package org.springdemobot.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springdemobot.model.User;
 import org.springdemobot.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.RestrictChatMember;
+import org.telegram.telegrambots.meta.api.objects.ChatPermissions;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.Timestamp;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -24,6 +30,25 @@ public class UserService {
                     new Timestamp(System.currentTimeMillis())
             );
             userRepository.save(user);
+        }
+    }
+
+    public void muteUser(TelegramLongPollingBot bot, Long userId, Long chatId, int seconds) {
+        RestrictChatMember muteUser = new RestrictChatMember();
+        muteUser.setChatId(String.valueOf(chatId));
+        muteUser.setUserId(userId);
+
+        ChatPermissions permissions = new ChatPermissions();
+        permissions.setCanSendMessages(false);
+        muteUser.setPermissions(permissions);
+
+        int untilDate = (int) (System.currentTimeMillis() / 1000) + seconds;
+        muteUser.setUntilDate(untilDate);
+
+        try {
+            bot.execute(muteUser);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
         }
     }
 }
