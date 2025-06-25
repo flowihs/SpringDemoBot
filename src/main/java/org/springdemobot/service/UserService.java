d51ae1e58ced0cf484f8acf169d495275dddde70
Zaraza-it <egor76522@Inbox.ri>
2025-06-25 08:13:20 +0300

    update project

--- src/main/java/org/springdemobot/service/UserService.java
+++ src/main/java/org/springdemobot/service/UserService.java
@@ -1,29 +1,35 @@
 package org.springdemobot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springdemobot.enums.Role;
 import org.springdemobot.model.User;
 import org.springdemobot.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    public void registerUserIfNew(Message message) {
-        Long chatId = message.getChatId();
-        if (userRepository.findById(chatId).isEmpty()) {
-            User user = new User(
-                    chatId,
+    public User registerUserIfNew(Update update, Message message) {
+        Long userId = update.getMessage().getFrom().getId();
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            User entity = new User(
+                    userId,
                     message.getChat().getFirstName(),
                     message.getChat().getLastName(),
                     message.getChat().getUserName(),
+                    Role.ROLE_USER,
                     new Timestamp(System.currentTimeMillis())
             );
-            userRepository.save(user);
+            return userRepository.save(entity);
         }
+        return user.get();
     }
 }
