0e65958bffc54736aa45b36d2a4b85d267662d77
Zaraza-it <egor76522@Inbox.ri>
2025-06-30 14:35:10 +0300

    refactoring v1.2

--- src/main/java/org/springdemobot/service/MessageSenderService.java
+++ src/main/java/org/springdemobot/service/MessageSenderService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -12,7 +13,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class MessageSenderService {
     @Autowired
     @Lazy
-    private TelegramBot bot;
+    private TelegramLongPollingBot bot;
 
     public void sendMessage(long chatId, String text) {
         SendMessage message = new SendMessage();
